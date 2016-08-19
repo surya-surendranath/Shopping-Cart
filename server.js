4b946d7de772edfab3286a41db3b248d5f6d7e6d
surya <surya.snath08@gmail.com>
2016-08-19 13:56:48 +1200

    node.js server installed

---
+++ server.js
@@ -0,0 +1,12 @@
+'use strict'
+ 
+var express = require('express');
+ 
+var app = express();
+
+app.use('/public', express.static(__dirname + '/public'));
+app.use('/node_modules', express.static(__dirname + '/node_modules'));
+
+app.listen('3000',function(){
+    console.log('Server running at http://localhost:3000 !!')
+})
