37da6ced70198f06e275883e90118e6b8a5c3e9b
surya <surya.snath08@gmail.com>
2016-08-19 16:04:35 +1200

    cart.js file define route

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Shopping-Cart
-Shopping Cart using AngularJS
+Angular-Bootstrap App
---
+++ public/cart/cart.js
@@ -0,0 +1,13 @@
+angular.module('cart', ['ngRoute'])
+
+.config(['$routeProvider', function($routeProvider) {
+	$routeProvider.when('/cart', {
+		templateUrl:'public/cart/cart.html' ,
+		controller:'CartCtrl'
+
+	})
+}])
+
+.controller('CartCtrl',[function() {
+	
+}])
